8ada9719fd276735fc1412dda5e0175e97ba1ca8
Iván Lynch <ivan.lynch@outlook.com.ar>
2020-12-10 23:24:03 -0300

    chore: release 1.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,9 @@
-<a name="unreleased"></a>
+## [1.0.3](https://github.com/ivanlynch/oshpro-report-incident/compare/1.0.0...1.0.3) (2020-12-11)
 
-## [Unreleased]
 
-<a name="1.0.0"></a>
+### Reverts
 
-## 1.0.0 - 2020-12-10
+* Revert "Added some changes" ([b434b84](https://github.com/ivanlynch/oshpro-report-incident/commit/b434b841696c47b7279284ad6a6b7b3b41415b35))
 
-[unreleased]: https://github.com/ivanlynch/oshpro-report-incident/compare/1.0.0...HEAD
+
+
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,4 @@
-import {task, series, src, dest} from 'gulp';
+import { task, series, src, dest } from 'gulp';
 import download from 'gulp-download';
 import { execSync } from 'child_process';
 import unzip from 'gulp-unzip';
@@ -7,57 +7,151 @@ import del from 'del';
 import ui5preload from 'gulp-ui5-preload';
 import uglify from 'gulp-uglify';
 import gulpif from 'gulp-if';
+const conventionalRecommendedBump = require('conventional-recommended-bump');
+const conventionalGithubReleaser = require('conventional-github-releaser');
+const execa = require('execa');
+const fs = require('fs');
+const { promisify } = require('util');
+const dotenv = require('dotenv');
+
+const result = dotenv.config();
+
+if (result.error) {
+  throw result.error;
+}
 
 let CONFIG = {
-    URL: ''
+  PRESET: 'angular',
+  STDIO: 'inherit',
 };
 
-task('CLEAN_FOLDER', done => {
-    del(['./runtime.zip', './www/resources']).then(() => {
-        done()
-    })
-})
-
-task('GET_LATEST_MOBILE_RUNTINE', done => {
-
-    CONFIG.URL = execSync(`curl -s https://api.github.com/repos/SAP/openui5/releases/latest | grep browser_download_url | grep openui5-runtime-mobile | cut -d '"' -f 4`).toString();
-    
-    download(CONFIG.URL)
-        .pipe(dest('./'))
-        .pipe(rename(function(path){
-            path.basename = "runtime"
-            path.extname = ".zip"
-        }))
-        .pipe(dest('./'))
-        .on('end', () => {
-            done()
-        })
+task('BUMP_VERSION', (done) => {
+  conventionalRecommendedBump(
+    {
+      preset: `${CONFIG.PRESET}`,
+    },
+    (error, recommended) => {
+      execa(
+        'npm',
+        ['version', recommended.releaseType, '--no-git-tag-version'],
+        {
+          stdio: `${CONFIG.STDIO}`,
+        },
+      ).then((result) => {
+        done();
+      });
+    },
+  );
 });
 
-task('CREATE_RESOURCE_FOLDER', done => {
-    src('./runtime.zip')
-        .pipe(unzip())
-        .pipe(dest('./www/'))
-        .on('end', () => {
-            del(['./runtime.zip', './openui5-runtime-mobile**'], done())
-        })
-})
+task('CHANGELOG', (done) => {
+  execa(
+    'npx',
+    [
+      'conventional-changelog',
+      '--preset',
+      CONFIG.PRESET,
+      '--infile',
+      'CHANGELOG.md',
+      '--same-file',
+    ],
+    { stdio: `${CONFIG.STDIO}` },
+  ).then((result) => {
+    done();
+  });
+});
 
-task('DELETE_DEBUG_FILES', done => {
-    del(['./www/**-dbg.js', './www/**/**-dbg.js']).then(() => {
-        done();
-    })
-})
+task('COMMIT_TAG_PUSH', (done) => {
+  // Read package.json
+  fs.readFile('package.json', (err, file) => {
+    const { version } = JSON.parse(file);
+    const commitMsg = `chore: release ${version}`;
+    execa('git', ['add', '.'], {
+      stdio: `${CONFIG.STDIO}`,
+    }).then((commit) => {
+      execa('git', ['commit', '--message', commitMsg], {
+        stdio: `${CONFIG.STDIO}`,
+      }).then((commitmsg) => {
+        execa('git', ['tag', `v${version}`], { stdio: `${CONFIG.STDIO}` }).then(
+          (tag) => {
+            execa('git', ['push', '--follow-tags'], {
+              stdio: `${CONFIG.STDIO}`,
+            }).then((result) => {
+              done();
+            });
+          },
+        );
+      });
+    });
+  });
+  //   const { version } = JSON.parse(await promisify(fs.readFile)('package.json'));
+  //   const commitMsg = `chore: release ${version}`;
+  //   await execa('git', ['add', '.'], { stdio });
+  //   await execa('git', ['commit', '--message', commitMsg], { stdio });
+  //   await ;
+  //   await execa('git', ['push', '--follow-tags'], { stdio });
+});
+
+task('CLEAN_FOLDER', (done) => {
+  del(['./runtime.zip', './www/resources']).then(() => {
+    done();
+  });
+});
+
+task('GET_LATEST_MOBILE_RUNTINE', (done) => {
+  CONFIG.URL = execSync(
+    `curl -s https://api.github.com/repos/SAP/openui5/releases/latest | grep browser_download_url | grep openui5-runtime-mobile | cut -d '"' -f 4`,
+  ).toString();
+
+  download(CONFIG.URL)
+    .pipe(dest('./'))
+    .pipe(
+      rename(function (path) {
+        path.basename = 'runtime';
+        path.extname = '.zip';
+      }),
+    )
+    .pipe(dest('./'))
+    .on('end', () => {
+      done();
+    });
+});
+
+task('CREATE_RESOURCE_FOLDER', (done) => {
+  src('./runtime.zip')
+    .pipe(unzip())
+    .pipe(dest('./www/'))
+    .on('end', () => {
+      del(['./runtime.zip', './openui5-runtime-mobile**'], done());
+    });
+});
+
+task('DELETE_DEBUG_FILES', (done) => {
+  del(['./www/**-dbg.js', './www/**/**-dbg.js']).then(() => {
+    done();
+  });
+});
 
 task('CREATE_COMPONENT_PRELOAD', () => {
-    return src([
-        'www/**/**.+(js|xml)', '!www/resources/**', '!www/Component-preload.js', '!gulpfile.js'
-    ])
-        .pipe(gulpif('www/**/*.js', uglify()))
-        .pipe(ui5preload({
-            base : '.', namespace : 'com/ws'
-        }))
-        .pipe(dest('www'))
+  return src([
+    'www/**/**.+(js|xml)',
+    '!www/resources/**',
+    '!www/Component-preload.js',
+    '!gulpfile.js',
+  ])
+    .pipe(gulpif('www/**/*.js', uglify()))
+    .pipe(
+      ui5preload({
+        base: '.',
+        namespace: 'com/ws',
+      }),
+    )
+    .pipe(dest('www'));
 });
 
-export default series('CLEAN_FOLDER', 'GET_LATEST_MOBILE_RUNTINE', 'CREATE_RESOURCE_FOLDER', 'CREATE_COMPONENT_PRELOAD')
+export default series(
+  'CLEAN_FOLDER',
+  'GET_LATEST_MOBILE_RUNTINE',
+  'CREATE_RESOURCE_FOLDER',
+  'CREATE_COMPONENT_PRELOAD',
+);
